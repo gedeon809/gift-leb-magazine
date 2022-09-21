1b5d7b40ff5ee7ad67da9e446fdf1b69daf7b8fd
Gedeon Kakona <gedeonkakona@gmail.com>
2022-09-21 14:32:58 +0200

    building frontend of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import TopBar from './components/topbar/TopBar';
+import Homepage from './pages/homepage/Homepage';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Settings from './pages/settings/Settings';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const user = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <TopBar />
+        <Routes>
+          <Route path="/" exact element={<Homepage />}></Route>
+          <Route
+            path="/register"
+            element={user ? <Homepage /> : <Register />}
+          ></Route>
+          <Route
+            path="/login"
+            element={user ? <Homepage /> : <Login />}
+          ></Route>
+          <Route
+            path="/write"
+            element={user ? <Write /> : <Register />}
+          ></Route>
+          <Route
+            path="/settings"
+            element={user ? <Settings /> : <Register />}
+          ></Route>
+          <Route path="/post/:postId" element={<Single />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
