3f689657d095ec6ce2529da06b8280aa17a6ff9a
Gedeon Kakona <gedeonkakona@gmail.com>
2022-09-21 22:00:30 +0200

    responsiveness

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './topbar.css';
 
 export default function TopBar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   const user = true;
   return (
     <div className="top">
       <div className="topLeft">
         <i className="topIcon fab fa-facebook-square"></i>
         <i className="topIcon fab fa-instagram-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-twitter-square"></i>
+        <button
+          className="hamburger"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          {/* icon from Heroicons.com */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
       </div>
-      <div className="topCenter">
+      <div className={isNavExpanded ? 'topCenter expanded' : 'topCenter'}>
         <ul className="topList">
           <li className="topListItem">
             <Link className="link" to="/">
